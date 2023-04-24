0db9f2ded28510e4576a5f2298c807679ac1b048
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-04-24 21:14:27 +0500

    Add navigation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navigation from './components/Navigation';
+import Home from './components/Home';
+import Details from './components/Details';
 
 function App() {
   return (
-    <div className="App" />
+    <div className="App">
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Details" element={<Details />} />
+      </Routes>
+    </div>
   );
 }
 
