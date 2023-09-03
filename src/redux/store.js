73f28797a984024d7fafe565f7a90bc706c5610b
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-09-03 07:09:52 +0500

    Add the english translation to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import chaptersReducer from './chapters/chaptersSlice';
+import englishSlice from './english/englishSlice';
 
 const store = configureStore({
   reducer: {
     chapters: chaptersReducer,
+    english: englishSlice
   },
 });
 
