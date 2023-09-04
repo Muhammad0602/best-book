29c7acbaf5e5abfce730271a1beb14e5a7ba9dd1
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-09-04 06:23:19 +0500

    Update Details.js, add the english translation to each ayah

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,14 +1,22 @@
 import React, { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
 import { BiArrowBack } from 'react-icons/bi';
+import { getEnglish } from '../redux/english/englishSlice';
 
 function Details() {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const { number } = useParams();
   const id = parseInt(number, 10);
   const { chapters } = useSelector((store) => store.chapters);
+  const { english } = useSelector((store) => store.english);
   const surah = chapters.length > 1 ? chapters[id - 1] : chapters;
+  const surahEng = english && english[id-1];
+
+  useEffect(() => {
+    dispatch(getEnglish());
+  }, [dispatch])
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -30,8 +38,12 @@ function Details() {
         </span>
       </h2>
       <article className="ayahs">
-        {surah.ayahs.map((ayah) => (
-          <p className="ayah" key={ayah.number}>{ayah.text}</p>
+        {surah.ayahs?.map((ayah, index) => (
+          <div key={ayah.number}>
+             <p className="ayah">{ayah.text}</p>
+             <p className="ayah english-ayah">{surahEng?.ayahs[index]?.text}</p>
+          </div>
+         
         ))}
       </article>
     </div>
